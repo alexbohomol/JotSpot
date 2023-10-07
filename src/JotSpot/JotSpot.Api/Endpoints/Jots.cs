5f825bceba488e2e904623680d1a29c4c6a9c076
Alex Bohomol <alexander.bogomol@gmail.com>
2023-10-07 13:02:08 +0300

    Remove `JotHandlers`

--- src/JotSpot/JotSpot.Api/Endpoints/Jots.cs
+++ src/JotSpot/JotSpot.Api/Endpoints/Jots.cs
@@ -1,4 +1,5 @@
-using static JotSpot.Api.Handlers.JotHandlers;
+using JotSpot.Api.Handlers;
+using JotSpot.Api.Models;
 
 namespace JotSpot.Api.Endpoints;
 
@@ -15,5 +16,50 @@ public static class Jots
         jots.MapPut("{id}", UpdateJot);
 
         jots.RequireAuthorization();
+    }
+
+    private static IResult GetAllJots(IRepository repository)
+    {
+        var jots = repository.GetAll().Select(JotResponse.FromDomain).ToArray();
+        return Results.Ok(jots);
+    }
+
+    private static IResult UpdateJot(Guid id, JotRequest request, IRepository repository)
+    {
+        var jot = repository.GetById(id);
+        if (jot is null)
+        {
+            return Results.NotFound();
+        }
+
+        jot.Title = request.Title;
+        jot.Text = request.Text;
+
+        return Results.Ok(JotResponse.FromDomain(jot));
+    }
+
+    private static IResult CreateJot(JotRequest request, IRepository repository)
+    {
+        var jot = new Jot { Id = Guid.NewGuid(), Title = request.Title, Text = request.Text };
+
+        repository.Add(jot);
+
+        return Results.Created($"/jots/{jot.Id}", new JotResponse(jot.Id, jot.Title, jot.Text));
+    }
+
+    private static IResult GetJot(Guid id, IRepository repository)
+    {
+        var jot = repository.GetById(id);
+
+        return jot is null
+            ? Results.NotFound()
+            : Results.Ok(JotResponse.FromDomain(jot));
+    }
+
+    private static IResult DeleteJot(Guid id, IRepository repository)
+    {
+        return repository.Delete(id)
+            ? Results.NoContent()
+            : Results.NotFound();
     }
 }
